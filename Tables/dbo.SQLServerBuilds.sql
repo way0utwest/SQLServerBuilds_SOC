128d3012cde0192ca27cf9dfe37ea20da98b80cf
Steve.Jones <sjones@sqlservercentral.com>
2017-08-29 15:14:34 -0600

    Added the version to differentiate the various patches among SQL versions.

--- Tables/dbo.SQLServerBuilds.sql
+++ Tables/dbo.SQLServerBuilds.sql
@@ -4,6 +4,7 @@ CREATE TABLE [dbo].[SQLServerBuilds]
 [BuildNumber] [varchar] (30) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [BuildDescription] [varchar] (100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
 [BuildKBArticleNumber] [varchar] (50) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
-[BuildKBArticleURL] [varchar] (1000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+[BuildKBArticleURL] [varchar] (1000) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+[SQLServerVersion] [varchar] (25) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
 ) ON [PRIMARY]
 GO
